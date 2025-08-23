4d74b76d4c686ee3cf2fd60be16be457c0b6d435
Matthias Gianfelice <matthias@gianfelice.de>
2025-08-23 11:51:45 +0200

    Conditions-package moved

--- kampanile3/carillon/models/rule.py
+++ kampanile3/carillon/models/rule.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
-from ..conditions import build_condition
+from .conditions import build_condition
 from .song import Song
 from .striker import Striker
 
